3fb942828b5911c818f38e689db8f9c2c2185124
Leandro Ruzicki <lruzicki@hotmail.com>
2023-07-11 10:49:01 -0300

    cambios html

--- project/apps/usuario/forms.py
+++ project/apps/usuario/forms.py
@@ -1,7 +1,25 @@
 from django import forms
 from apps.usuario.models import Usuario
+from django.shortcuts import render
 
 class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = ['nombre', 'email']
+
+def usuarioFormulario(request):
+ 
+      if request.method == "POST":
+ 
+            miFormulario = UsuarioForm(request.POST)
+            print(miFormulario)
+ 
+            if miFormulario.is_valid:
+                  informacion = miFormulario.cleaned_data
+                  usuario = Usuario(nombre=informacion["nombre"], email=informacion["email"])
+                  usuario.save()
+                  return render(request, "Home/base.html")
+      else:
+            miFormulario = UsuarioForm()
+ 
+      return render(request, "usuario/crear.html", {"miFormulario": miFormulario})
