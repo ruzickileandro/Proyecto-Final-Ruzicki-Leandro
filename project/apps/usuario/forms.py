e0be4e448de493991011f97bea8ef842cb87e589
Leandro Ruzicki <lruzicki@hotmail.com>
2023-08-03 15:21:50 -0300

    agrego mensaje en register

--- project/apps/usuario/forms.py
+++ project/apps/usuario/forms.py
@@ -4,32 +4,6 @@ from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
-"""class UsuarioForm(forms.ModelForm):
-    class Meta:
-        model = Usuario
-        fields = ['nombre', 'apellido', 'usuario', 'email', 'contraseña']"""
-
-"""def usuarioFormulario(request):
- 
-      if request.method == "POST":
- 
-            miFormulario = UsuarioForm(request.POST)
-            print(miFormulario)
- 
-            if miFormulario.is_valid:
-                  informacion = miFormulario.cleaned_data
-                  usuario = Usuario(nombre=informacion["nombre"],
-                                   apellido=informacion["apellido"],
-                                   usuario=informacion["usuario"],
-                                   email=informacion["email"],
-                                   contraseña=informacion["contraseña"])
-                  usuario.save()
-                  return render(request, "Home/base.html")
-      else:
-            miFormulario = UsuarioForm()
- 
-      return render(request, "usuario/crear_usuario.html", {"miFormulario": miFormulario})"""
-
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
         model = User
--- project/apps/usuario/views.py
+++ project/apps/usuario/views.py
@@ -3,23 +3,12 @@ from django.http import HttpResponse, HttpRequest
 from django.contrib.auth import login, authenticate
 from apps.usuario.models import Usuario
 from apps.evento.models import Evento
-#from .forms import UsuarioForm
 
 from . import forms
 
 def lista_usuarios(request):
     usuarios = Usuario.objects.all()
     return render(request, 'usuario/index.html', {'usuarios': usuarios})
-
-"""def crear_usuario(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
-        form = UsuarioForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("Home:home")
-    else: 
-        form = UsuarioForm()
-    return render(request, "usuario/crear_usuario.html", {"form": form})"""
 
 def login_request(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
@@ -39,9 +28,10 @@ def register(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = forms.CustomUserCreationForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data["username"]
             form.save()
-            return render(request, "Home/base.html", {"mensaje": "Usuario creado"})
+            username = form.cleaned_data["username"]
+            mensaje = f"Usuario {username} creado"
+            return render(request, "usuario/register.html", {"form": form, "mensaje": mensaje})
     else:
         form = forms.CustomUserCreationForm()
     return render(request, "usuario/register.html", {"form": form})
